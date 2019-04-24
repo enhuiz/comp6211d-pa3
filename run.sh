66da5e831f3c52c25329b2065ede50299fe18e8d
enhuiz <niuzhe.nz@outlook.com>
2019-04-24 10:05:53 +0800

    add opts

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-python3 -u ./vanilla_gan.py 1> log/vanilla_gan.out &
-python3 -u ./cycle_gan.py 1> log/cycle_gan.out &
+python3 -u ./vanilla_gan.py --num_epochs 60 1> log/vanilla_gan.out &
+python3 -u ./cycle_gan.py --train_iters 10000 1> log/cycle_gan.out &
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
